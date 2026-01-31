e9a2b80aea8a2c1a24dedbced4b01126898b5462
ranio88smb88-hub <ranio88.smb88@gmail.com>
2026-01-31 11:34:42 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Konfigurasi Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDEXAMPLEEXAMPLEEXAMPLE",
+  authDomain: "izin-staff-app.firebaseapp.com",
+  projectId: "izin-staff-app",
+  storageBucket: "izin-staff-app.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdef123456"
+};
